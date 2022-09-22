80326d09b4a6e19ac2d78fc55eb552eb12b74675
Shivkumar54 <mshivkumar59@gmail.com>
2022-09-22 21:42:22 +0530

    Login Page done

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,10 +1,42 @@
 import React from "react"
+import "./login.css"
+
+import LoginLogo from "../../Images/login.svg"
 
 const Login = () => {
   return (
-    <div className="DefaultContentHolder">
-     <h1>Login</h1>
-     User Login here
+    <div className="DefaultContentHolder container">
+      <div className="LoginWrapper">
+        <div className="loginImage">
+          <img src={LoginLogo} alt="" />
+        </div>
+        <div className="loginContent">
+          <h2 className="lHeading">Hello Welcome Back</h2>
+          <form>
+            <label htmlFor="">Enter UserName</label>
+            <br />
+            <input
+              className="lInputer"
+              type="text"
+              placeholder="eg - johnWick@456"
+            />
+            <br />
+            <label htmlFor="">Enter Password</label>
+            <br />
+            <input
+              className="lInputer"
+              type="text"
+              placeholder="At least 8 characters"
+            />
+            <br />
+            <a href="#" className="forgot">
+              Forgot Password?
+            </a>
+            <br />
+            <button className="lButton">Get Me In</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
