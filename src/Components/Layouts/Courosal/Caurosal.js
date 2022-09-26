39572ab10e9a633e7194a6adc287ad48d174e9fc
Shivkumar54 <mshivkumar59@gmail.com>
2022-09-26 15:27:30 +0530

    Ad78cau

--- src/Components/Layouts/Courosal/Caurosal.js
+++ src/Components/Layouts/Courosal/Caurosal.js
@@ -5,45 +5,48 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
 const Caurosal = () => {
- const settings = {
-   arrows: true,
-   centerMode: true,
-   dots: false,
-   infinite: true,
-   speed: 500,
-   slidesToShow: 1,
-   slidesToScroll: 1,
-   centerPadding: "100px",
-   responsive: [
-     {
-       breakpoint: 1024,
-       settings: {
-         slidesToShow: 1,
-         slidesToScroll: 1,
-         infinite: true,
-         dots: false,
-         centerPadding: "20px",
-       },
-     },
-     {
-       breakpoint: 600,
-       settings: {
-         slidesToShow: 1,
-         slidesToScroll: 1,
-         initialSlide: 2,
-         centerPadding: "0px",
-       },
-     },
-     {
-       breakpoint: 480,
-       settings: {
-         slidesToShow: 1,
-         slidesToScroll: 1,
-         centerPadding: "0px",
-       },
-     },
-   ],
- }
+  const settings = {
+    arrows: true,
+    centerMode: true,
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    centerPadding: "100px",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          centerPadding: "30px",
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+          centerPadding: "30px",
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "30px",
+        },
+      },
+    ],
+  }
 
   const list = [
     {
